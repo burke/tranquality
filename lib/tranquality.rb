197f2e94d39b9b1d488f9932e5db15b666249778
Burke Libbey <burke@burkelibbey.org>
2011-11-30 11:42:27 -0600

    reorder requires to prevent a monkeypatch from getting clobbered

--- lib/tranquality.rb
+++ lib/tranquality.rb
@@ -1,7 +1,10 @@
+require 'ruby_parser'
+require 'sexp_processor'
+
 require 'flay'
 require 'flog'
-require 'tranquality/runner'
 
+require 'tranquality/runner'
 require 'tranquality/sexp_extensions'
 
 module Tranquality
